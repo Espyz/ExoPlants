8ed189fb746e47ae9f10fb5d1078958eb3049326
Espyz <truharandrey1015@gmail.com>
2024-11-28 20:27:41 +0500

    Копия

---
+++ main.js
@@ -0,0 +1,56 @@
+const nextButton = document.getElementById('strelka-right');
+const prevButton = document.getElementById('strelka-left');
+const cards = document.getElementsByClassName('card-1');
+const lenSlider = cards.length;
+
+nextButton.addEventListener('click', nextElem);
+prevButton.addEventListener('click', prevElem);
+let currentPage = 0;
+prevButton.inert = true;
+function nextElem() {
+    currentPage += 1;
+    nextButton.inert = true;
+    prevButton.inert = true;
+    setTimeout(enableButtons, 500, prevButton, nextButton);
+    for (let card of cards) {
+
+        const leftRange = parseInt(getComputedStyle(card).translate);
+        const widthElem = parseInt(card.offsetWidth + 20);
+        console.log(leftRange, widthElem)
+        if (leftRange) {
+            card.style = `translate: ${leftRange - widthElem}px`;
+            continue;
+        }
+        card.style = `translate: -${widthElem}px`;
+    }
+}
+function prevElem() {
+    currentPage -= 1;
+    nextButton.inert = true;
+    prevButton.inert = true;
+    setTimeout(enableButtons, 500, prevButton, nextButton);
+
+    for (let card of cards) {
+        const leftRange = parseInt(getComputedStyle(card).translate);
+        const widthElem = parseInt(card.offsetWidth + 20);
+        if (leftRange) {
+            card.style = `translate: ${leftRange + widthElem}px`;
+            continue;
+        }
+        card.style = `translate: +${widthElem}px`;
+    }
+}
+function enableButtons(prev, next) {
+    if (currentPage === 0) {
+        prev.inert = true;
+        next.inert = false;
+        return;
+    }
+    if (currentPage === lenSlider - 4) {
+        next.inert = true;
+        prev.inert = false;
+        return;
+    }
+    prev.inert = false;
+    next.inert = false;
+}
