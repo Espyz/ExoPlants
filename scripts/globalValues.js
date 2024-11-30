24fd45832b07f66b739f843137bf78bc325e5b31
Espyz <truharandrey1015@gmail.com>
2024-11-30 09:28:05 +0500

    Сделал динамическую страницу good-card

--- scripts/globalValues.js
+++ scripts/globalValues.js
@@ -2,8 +2,8 @@ const cardsList = Array.from(document.querySelectorAll('.card-1'));
 
 // Значения для заполнения good-card.html
 const selectedCard = {
-    name: 'ПАХИРА',
-    priceLow: '1608 Р',
+    name:      'ПАХИРА',
+    priceLow:  '1608 Р',
     priceHigh: '3285 Р',
     photoLink: '../IMG/Pahira-IMG.svg',
 };
@@ -15,17 +15,18 @@ for (const card of cardsList) {
 }
 
 function openGoods(card, photoLink, event) {
-    const mainFile = event.view.location.pathname.split('/').at(-1)
+    const mainFile = event.view.location.pathname.split('/').at(-1);
     const elementName = Array.from(card.querySelectorAll('p'));
     selectedCard.name = elementName[0]?.innerText;
     selectedCard.priceLow = elementName[1]?.innerText;
     selectedCard.priceHigh = elementName[2]?.innerText;
     selectedCard.photoLink = photoLink;
+    localStorage.setItem('selectedCard', JSON.stringify(selectedCard));
     
     if (mainFile === 'index.html') {
-        location.replace('./pages/good-card.html')
+        location.replace('./pages/good-card.html');
     }
     else {
-        location.replace('./good-card.html')
+        location.replace('./good-card.html');
     }
 }
--- scripts/good-card.js
+++ scripts/good-card.js
@@ -4,23 +4,43 @@ if (counters) {
     counters.forEach(quantity => {
         quantity.addEventListener('click', e => {
             const target = e.target;
-
-            if(target.closest('.counter__button')) {
+            
+            if (target.closest('.counter__button')) {
                 let value = parseInt(target.closest('.quantity').querySelector('input').value);
-
-                if(target.classList.contains('plus-btn')) {
+                
+                if (target.classList.contains('plus-btn')) {
                     value++;
-                } else {
+                }
+                else {
                     --value;
                 }
                 if (value <= 0) {
                     value = 0;
                     
                 }
-
+                
                 target.closest('.quantity').querySelector('input').value = value;
             }
+            
+        });
+    });
+}
 
-        })
-    })
+const selectedCard = JSON.parse(localStorage.getItem('selectedCard'));
+
+if (selectedCard.photoLink) {
+    const image = Array.from(document.querySelectorAll('.img-main'))[0];
+    const name = Array.from(document.querySelectorAll('.center'))[0];
+    const prices = Array.from(document.querySelectorAll('.price p'));
+    
+    image.src = selectedCard.photoLink;
+    name.innerText = selectedCard.name;
+    prices[0].innerText = selectedCard.priceLow;
+    
+    if (selectedCard.priceHigh) {
+        prices[1].innerText = selectedCard.priceHigh
+    }
+    else {
+        prices[1].style.display = 'none'
+    }
 }
